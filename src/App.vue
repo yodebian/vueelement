d3e71e1b21d71795bdb53920bc3855576e7b69d8
Ricardo Toledo <ricardotoledo@MBP-de-Ricardo.iptv.microsoft.com>
2017-09-01 19:32:34 -0300

    NUEVO

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
-  <div id="app">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">Processing Center</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">Workspace</template>
-        <el-menu-item index="2-1">item one</el-menu-item>
-        <el-menu-item index="2-2">item two</el-menu-item>
-        <el-menu-item index="2-3">item three</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="3">
-        <a href="https://www.ele.me" target="_blank">Orders</a>
-      </el-menu-item>
-    </el-menu>
+  <div id="app" v-loading.fullscreen.lock="fullscreenLoading" element-loading-text="Loading...">
+    <el-row :gutter="24" >
+      <div class="grid-content bg-blue">
+        <div class="encuadra">
+          <el-row>
+            <el-col :span="2" class="is-hidden-touch">
+              <div class="logo">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/76/Slack_Icon.png" width="40" height="40" />
+              </div>
+            </el-col>
+            <el-col :span="5" class="is-hidden-touch">
 
+              <div class="logo">
+                </h3>
+                <h3> Element- Custom Theme</h3>
+              </div>
+            </el-col>
+            <el-col :span="17" style="margin-top: 0px">
+              <div class="menu">
+                <el-menu class="bg-blue-ns" mode="horizontal" >
+                  <el-menu-item index="1" >Processing Center</el-menu-item>
+                  <el-submenu index="2" >
+                    <template slot="title" >Workspace</template>
+                    <el-menu-item index="2-1">item one</el-menu-item>
+                    <el-menu-item index="2-2">item two</el-menu-item>
+                    <el-menu-item index="2-3">item three</el-menu-item>
+                  </el-submenu>
+                </el-menu>
+
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+    </el-row>
+    <!-- <el-row :gutter="24">
+            <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+              <el-menu-item index="1">Processing Center</el-menu-item>
+              <el-submenu index="2">
+                <template slot="title">Workspace</template>
+                <el-menu-item index="2-1">item one</el-menu-item>
+                <el-menu-item index="2-2">item two</el-menu-item>
+                <el-menu-item index="2-3">item three</el-menu-item>
+              </el-submenu>
+             
+            </el-menu>-->
+    </el-row>
     <el-row :gutter="24">
       <el-col :span="24">
         <div class="grid-content bg-purple">
@@ -48,6 +81,9 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js and element + theme',
+      activeIndex: '1',
+      activeIndex2: '1',
+      fullscreenLoading:false,
       tableData: [{
         date: '2016-05-03',
         name: 'Tom',
@@ -65,13 +101,29 @@ export default {
         name: 'Tom',
         address: 'No. 189, Grove St, Los Angeles'
       }]
+      
+      
+
     }
+  },
+  mounted(){
+    this.openFullScreen()
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    openFullScreen() {
+        this.fullscreenLoading = true;
+        setTimeout(() => {
+          this.fullscreenLoading = false;
+        }, 3000);
+      }
   }
 }
 </script>
 <style>
 .el-row {
-  margin-top: 20px;
   margin-bottom: 20px;
   &:last-child {
     margin-bottom: 0;
@@ -102,5 +154,44 @@ export default {
 .row-bg {
   padding: 10px 0;
   background-color: #f9fafc;
+}
+
+.bg-blue {
+  height: 80px;
+  background-color: #1b88d9;
+  color: white;
+  font-size: 16px;
+  font-weight: lighter;
+}
+
+.bg-blue-ns {
+  background-color: #1b88d9;
+  line-height: 100px;
+  font-size: 16px;
+  
+}
+
+
+
+.logo {
+  padding: 0px 20px;
+}
+
+.encuadra {
+  padding: 10px;
+}
+
+.encuadra i {
+  margin-right: 5px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 30px;
+}
+
+.logo {
+  border-right: 1px solid #ffffff;
+  margin-top: 10px;
 }
 </style>
